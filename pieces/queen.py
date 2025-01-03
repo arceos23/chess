2e6baed4da433f56f64d0760975150a4148be910
Shane Arceo <arceoshane23@gmail.com>
2025-01-03 17:45:27 -0500

    fixed bug of other directional moves returning True for the actual directional move that should be tested

--- pieces/queen.py
+++ pieces/queen.py
@@ -1,5 +1,11 @@
 from .piece import Piece
-from .moves import can_move_vertically, can_move_horizontally, can_move_diagonally
+from .moves import (
+    get_dx,
+    get_dy,
+    can_move_vertically,
+    can_move_horizontally,
+    can_move_diagonally,
+)
 import constants
 
 
@@ -8,15 +14,27 @@ class Queen(Piece):
     def __str__(self):
         return f"{constants.QUEEN}{self.get_color()}"
 
+    def __is_vertical_move(self, file, rank):
+        return abs(get_dx(self.rank, rank)) == 1 and get_dy(self.file, file) == 0
+
+    def __is_horizontal_move(self, file, rank):
+        return get_dx(self.rank, rank) == 0 and abs(get_dy(self.file, file)) == 1
+
+    def __is_diagonal_move(self, file, rank):
+        return abs(self.file - file) == abs(self.rank - rank)
+
     def can_move(self, chessboard, file, rank):
         return (
-            can_move_vertically(
+            self.__is_vertical_move(file, rank)
+            and can_move_vertically(
                 chessboard, self.file, self.rank, file, rank, self.is_white
             )
-            or can_move_horizontally(
+            or self.__is_horizontal_move(file, rank)
+            and can_move_horizontally(
                 chessboard, self.file, self.rank, file, rank, self.is_white
             )
-            or can_move_diagonally(
+            or self.__is_diagonal_move(file, rank)
+            and can_move_diagonally(
                 chessboard, self.file, self.rank, file, rank, self.is_white
             )
         )
