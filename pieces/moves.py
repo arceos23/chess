9866946d77d6bd6d22867ba0de1502c2368cf491
Shane Arceo <arceoshane23@gmail.com>
2025-01-03 22:21:03 -0500

    fixed bug in queen and associated movements

--- pieces/moves.py
+++ pieces/moves.py
@@ -38,7 +38,7 @@ def __is_same_file(start_file, end_file):
 
 
 def __is_same_rank(start_rank, end_rank):
-    return start_rank != end_rank
+    return start_rank == end_rank
 
 
 def __can_place(chessboard, end_file, end_rank, placing_piece_is_white):
@@ -48,6 +48,10 @@ def __can_place(chessboard, end_file, end_rank, placing_piece_is_white):
         or end_piece.is_white != placing_piece_is_white
         and str(end_piece) != constants.KING
     )
+
+
+def get_direction(delta):
+    return 1 if delta > 0 else -1
 
 
 def get_dx(start_rank, end_rank):
@@ -93,13 +97,13 @@ def can_move_vertically(
     is_white,
     capture_allowed=True,
 ):
-    dx = get_dx(start_rank, end_rank)
+    x_move = get_direction(get_dx(start_rank, end_rank))
     return (
         not is_same_position(start_file, start_rank, end_file, end_rank)
         and __is_same_file(start_file, end_file)
         and all(
             chessboard.is_empty(end_file, rank)
-            for rank in range(start_rank + dx, end_rank, dx)
+            for rank in range(start_rank + x_move, end_rank, x_move)
         )
         and (
             capture_allowed
@@ -113,12 +117,22 @@ def can_move_vertically(
 def can_move_horizontally(
     chessboard, start_file, start_rank, end_file, end_rank, is_white
 ):
+    y_move = get_direction(get_dy(start_file, end_file))
+    print(
+        not is_same_position(start_file, start_rank, end_file, end_rank),
+        __is_same_rank(start_rank, end_rank),
+        all(
+            chessboard.is_empty(file, end_rank)
+            for file in range(start_file + y_move, end_file, y_move)
+        ),
+        __can_place(chessboard, end_file, end_rank, is_white),
+    )
     return (
         not is_same_position(start_file, start_rank, end_file, end_rank)
         and __is_same_rank(start_rank, end_rank)
         and all(
-            chessboard.is_empty(end_rank, file)
-            for file in range(start_file, end_file, get_dy(start_file, end_file))
+            chessboard.is_empty(file, end_rank)
+            for file in range(start_file + y_move, end_file, y_move)
         )
         and __can_place(chessboard, end_file, end_rank, is_white)
     )
@@ -127,21 +141,25 @@ def can_move_horizontally(
 def can_move_diagonally(
     chessboard, start_file, start_rank, end_file, end_rank, is_white
 ):
+    dx, dy = get_dx(start_rank, end_rank), get_dy(start_file, end_file)
+    x_direction, y_direction = get_direction(dx), get_direction(dy)
+    total_movement = abs(dx)
     return (
         not is_same_position(start_file, start_rank, end_file, end_rank)
         and abs(start_file - end_file) == abs(start_rank - end_rank)
         and all(
-            chessboard.is_empty(file, rank)
-            for rank in range(start_rank, end_rank, get_dx(start_rank, end_rank))
-            for file in range(start_file, end_file, get_dy(start_file, end_file))
+            chessboard.is_empty(
+                start_file + move * y_direction,
+                start_rank + move * x_direction,
+            )
+            for move in range(1, total_movement)
         )
         and __can_place(chessboard, end_file, end_rank, is_white)
     )
 
 
 def can_jump(chessboard, start_file, start_rank, end_file, end_rank, is_white):
-    dy = end_file - start_file
-    dx = end_rank - start_rank
+    dx, dy = get_dx(start_rank, end_rank), get_dy(start_file, end_file)
     return any(
         dx == move.dx and dy == move.dy for move in __VALID_JUMP_MOVES
     ) and __can_place(chessboard, end_file, end_rank, is_white)
--- pieces/queen.py
+++ pieces/queen.py
@@ -15,10 +15,10 @@ class Queen(Piece):
         return f"{constants.QUEEN}{self.get_color()}"
 
     def __is_vertical_move(self, file, rank):
-        return abs(get_dx(self.rank, rank)) == 1 and get_dy(self.file, file) == 0
+        return get_dx(self.rank, rank) != 0 and get_dy(self.file, file) == 0
 
     def __is_horizontal_move(self, file, rank):
-        return get_dx(self.rank, rank) == 0 and abs(get_dy(self.file, file)) == 1
+        return get_dx(self.rank, rank) == 0 and get_dy(self.file, file) != 0
 
     def __is_diagonal_move(self, file, rank):
         return abs(self.file - file) == abs(self.rank - rank)
